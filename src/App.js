936d74fe9950d72768c9b4d56f928e31555f0703
mikez33 <zhaom33@gmail.com>
2022-04-19 16:23:40 -0400

    added initial twitter work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
+} from 'react-twitter-embed';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +32,18 @@ function App() {
         >
           Learn React
         </a>
+        <TwitterTweetEmbed
+          tweetId={'1514813553177362434'}
+          options={{ height: 800 }}
+        />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="saurabhnemade"
+          options={{ height: 800 }}
+        />
+        <TwitterHashtagButton
+          tag={'imdb'}
+        />
       </header>
     </div>
   );
